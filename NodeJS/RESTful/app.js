50926528116164aac55b393f3e5cfcc43c402162
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-02-08 22:05:56 +0530

    comments app

--- NodeJS/RESTful/app.js
+++ NodeJS/RESTful/app.js
@@ -1,45 +1,86 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // middleware function
+app.use(methodOverride('_method')); // middleware function for method override
 
-const comments = [{
+let comments = [{
         id: 0,
         user: "john",
-        text: 'this is first comment'
+        text: "this is john's comment"
     },
     {
         id: 1,
         user: "doe",
-        text: 'this is second comment'
+        text: "this is doe's comment"
     },
     {
         id: 2,
         user: "Dennis",
-        text: 'this is third comment'
+        text: "this is Dennis's comment"
     },
     {
         id: 3,
         user: "Ritche",
-        text: 'this is fourth comment'
+        text: "this is Ritche's comment"
     }
 ];
 
+// list all comments
 app.get('/comments', (req, res) => {
     res.render('index', { comments });
 })
 
+// Display the form to create new comment
 app.get('/comments/new', (req, res) => {
     res.render('new');
 })
 
+// add new comment and redirect to /comments
 app.post('/comments', (req, res) => {
     const newComment = req.body;
     newComment.id = comments.length;
     comments.push(newComment);
+
+    res.redirect('/comments');
+})
+
+// show particular comment
+app.get('/comments/:commentid', (req, res) => {
+    const { commentid } = req.params;
+
+    const comment = comments.find((comment) => comment.id === parseInt(commentid));
+    res.render('onlyComment', { comment });
+})
+
+// edit particular comment
+app.get('/comments/:commentid/edit', (req, res) => {
+    const { commentid } = req.params;
+
+    const comment = comments.find((comment) => comment.id === parseInt(commentid));
+    res.render('edit', { comment });
+})
+
+// edit and save
+// npm install method-override
+app.patch('/comments/:commentid', (req, res) => {
+    const { commentid } = req.params;
+
+    const comment = comments.find((comment) => comment.id === parseInt(commentid));
+    comment.user = req.body.user;
+    comment.text = req.body.text;
+
+    res.redirect('/comments');
+})
+
+// delete comment
+app.delete('/comments/:commentid', (req, res) => {
+    comments = comments.filter((comm) => comm.id != parseInt(req.params.commentid));
+    res.redirect('/comments');
 })
 
 
